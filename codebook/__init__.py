026f25b13b47dc6787dc3fc390495fe4b5358c37
Tiago Seabra <tlgs@users.noreply.github.com>
2022-02-01 01:35:14 +0000

    docs: package docstring

--- codebook/__init__.py
+++ codebook/__init__.py
@@ -1,1 +1,18 @@
-__all__ = ["transposition", "substitution", "rsa"]
+"""
+## What's this?
+
+This is a *code along* to Simon Singh's
+[The Code Book](https://simonsingh.net/books/the-code-book/)
+(ISBN 978-1-85702-889-8).
+
+It implements every cipher that is described
+(and given *enough* attention) in the book.
+Only **encryption operations** are implemented.
+
+The package aims to provide a consistent API across all classical ciphers.
+
+## Disclaimer
+
+This project is meant purely as an exercise to absorb the contents of the book.
+**Should not be seen as a reference towards applying any cryptographic technique.**
+"""
